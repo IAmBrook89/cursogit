e67fc1436406c6fdc5e6539bcded82a4571314f3
Ivan <ivanlamuerte_15@hotmail.com>
2024-12-21 12:22:16 -0300

    inicio un proyecto con git

---
+++ Comandos.js
@@ -0,0 +1,3 @@
+//git init --incializa el repositorio en la carpeta donde estamos trabajando
+//git add "nombre de archivo" --agrega ese archivo al repositorio local
+//git add . --agrega todos los archivos al repositorio local
