b04b87e3d630ddf0ca00e4fb8a89f02c19b0f48d
Ivan <ivanlamuerte_15@hotmail.com>
2024-12-21 12:45:26 -0300

    final de clase 1

--- Comandos.js
+++ Comandos.js
@@ -1,3 +1,8 @@
 //git init --incializa el repositorio en la carpeta donde estamos trabajando
 //git add "nombre de archivo" --agrega ese archivo al repositorio local
 //git add . --agrega todos los archivos al repositorio local
+//git commit -m "mensaje" --Comitea los cambios al repo y el -m es para definir un mensaje
+//git log --muestra el log de los commit realizados
+// --al hacer cambios los archivos se veran con la M de modified, hay q volver a hacer un add para q remplace en el repo los archivos antes del commit.--
+//
+//
