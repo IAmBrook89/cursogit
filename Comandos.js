2db283d7161b80f2f03dee7db684f7c12574436f
Ivan <ivanlamuerte_15@hotmail.com>
2024-12-28 10:51:14 -0300

    commit

--- Comandos.js
+++ Comandos.js
@@ -4,5 +4,15 @@
 //git commit -m "mensaje" --Comitea los cambios al repo y el -m es para definir un mensaje
 //git log --muestra el log de los commit realizados
 // --al hacer cambios los archivos se veran con la M de modified, hay q volver a hacer un add para q remplace en el repo los archivos antes del commit.--
+//node ./nombre de archivo el entorno node ejecutara el archivo en nuestro equipo
+//git status te da la info de la rama donde estas trabajando y si hay cambios etc
+//git remote -v busca en el repo local la direccion del repositorio remoto si es que existe
+// -- para trabajar con el repo remoto siempre se usa la palabra remote--
+//git remote add origin "Url del repo" --agrega el repo remoto correspondiente
+//git push -u origin main --pushea el contenido al repo, git se va a fijar cada vez que haces un push va a pedir las credenciales de usuario, estando ya logueado y diciendo -u ya al estar logueado va a omitir el paso de las credenciales por q ya las tiene
+//git push --intenta realizar el push, se pediran credenciales
+//git branch --devuelve la rama donde estamos trabajando
+//
+//
 //
 //
